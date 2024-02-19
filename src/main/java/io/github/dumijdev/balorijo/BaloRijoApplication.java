808ee3b9a6cae5ceeb4202f97758608f8b614b35
Dumilde Fernando <dumildes.fernando@bancobai.ao>
2024-02-19 23:24:06 +0100

    second commit

---
+++ src/main/java/io/github/dumijdev/balorijo/BaloRijoApplication.java
@@ -0,0 +1,13 @@
+package io.github.dumijdev.balorijo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaloRijoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaloRijoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dumijdev/balorijo/BaloRijoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dumijdev.balorijo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaloRijoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
